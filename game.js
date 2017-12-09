0579887e971cbaaf8c46aca7b1cd323b45ed8d71
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-12-08 22:20:21 -0700

    constructing arrays of letters and underscores

--- game.js
+++ game.js
@@ -2,14 +2,49 @@
 // this will receive user input through the inquirer.prompt
 // letter will be stored in a letter array
 //The completed game should be able to receive user input using the inquirer or prompt npm packages.
+// each new letter guess should be pushed into an array that stores the letter in the position that directly corresponds to the randomly selected word, .sliced into an array itself
 
 var word = require("./word.js");
 var letter = require("./letter.js");
+var inquirer = require("inquirer");
 
 var wordBank = ["circle", "square", "math", "toast", "password", "pen", "pin", "pan", "pun", "gym", "roast", "most", "corner", "vertices", "fry", "slalom", "crust", "linear", "marshmellow", "reserved", "banana", "fork", "hang"];
+var letterDump = [];
 
-var wordFill = wordBank[Math.floor(Math.random() * wordBank.length)];
+function hangman(){
+    var wordFill = wordBank[Math.floor(Math.random() * wordBank.length)];
 
+    console.log(wordFill);
+    var wordSplit = wordFill.slice();
+    console.log(wordSplit);
+    console.log(wordSplit.length);
+    var underScore = [];
+    for (var i = 0; i < wordSplit.length; i++){
+        underScore.push("_");
+    }
+    console.log(underScore);
+    inquirer.prompt([
+        {
+        type: "input",
+        name: "userInput", 
+        message: "Guess a letter!" 
+        }
+    ]).then(answers => { 
+        var theLetter = answers.userInput;
+        console.log(theLetter);
 
+        // for (var i = 0; i < wordSplit.length; i++) {
+        //     if (theLetter === wordSplit[i]) {
+        //         letterDump.push(wordSplit[i])
+        //         console.log(letterDump);
+        //     }
+        //     else (){
 
-console.log(wordFill);
+        //     }
+        // }
+
+    });
+}
+
+hangman();
+// some inquirer prompt to leave the game, give up when number of guesses is used up or restart (like return to a function that includes the primary random word generation and inquirer guessing).
--- word.js
+++ word.js
@@ -2,11 +2,18 @@
 //Used to create an object representing the current word the user is attempting to guess.This should contain word specific logic and data.
 var letter = require("./letter.js")
 
+
+
 //word constructor function
 function Word(word) {
     this.word = word
 }
 
+//trying to remotely make the underscore generator as a prototype, export it and require in the game.js
+
+Word.prototoype.underGen = function(){
+    
+}
 console.log("word");
 
 
